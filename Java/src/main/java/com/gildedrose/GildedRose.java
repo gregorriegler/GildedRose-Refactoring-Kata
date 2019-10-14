eff863efff783d91964f7215942bc6cf017b6878
Gregor Riegler <gregor.riegler@ebcont.com>
2019-10-14 23:17:57 +0200

    yep

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,8 +12,7 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             item.quality = item.quality - 1;
@@ -58,28 +57,10 @@ class GildedRose {
                 }
             } else {
                 if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+
                 } else {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
-
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-                        }
                     }
                 }
 
@@ -89,15 +70,7 @@ class GildedRose {
 
                 if (item.sellIn < 0) {
                     if (!item.name.equals("Aged Brie")) {
-                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } else {
-                            item.quality = item.quality - item.quality;
-                        }
+                    
                     } else {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
